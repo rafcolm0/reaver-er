ccd576cae902aa1c77f0a007d8c80bda693b0e33
rafcolm0 <rafael.colon5@gmail.com>
2016-12-13 19:17:56 -0500

    iptabler param

--- iptabler_.py
+++ iptabler_.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 import os
-
+import sys
 
 def main():
-    file = open('iptabler_rules.txt', 'r')
-    lines = file.readlines()
-    for i in lines:
-        if i[0] != "#":
-            print("exec: " + i)
-            os.system(i)
+    if len(sys.argv) != 2:
+        print("Script that adds iptables listed on a formated file.\n\nRequired Argument:\n     <file>          Path of file containing rules \n\n Example: \n    pyhton3 iptabler_ iptable_rules.txt")
+        sys.exit()
+    RULES = sys.argv[1]
+    try:
+        file = open(RULES, 'r')
+        lines = file.readlines()
+        for i in lines:
+            if i[0] != "#":
+                print("exec: " + i)
+                os.system(i)
+    except OSError as e:
+        print("Script that adds iptables listed on a formated file.\n\nRequired Argument:\n     <file>          Path of file containing rules \n\n Example: \n    pyhton3 iptabler_ iptable_rules.txt")
+        sys.exit()
 
 
 if __name__ == '__main__':
